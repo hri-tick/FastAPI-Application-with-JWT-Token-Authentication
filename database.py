2b7cf1ac9816bef14f1937443deedaf23e2ac421
hri-tick <138192141+hri-tick@users.noreply.github.com>
2023-11-28 15:36:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,12 +4,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./FASTAPI_DB.db"
+#SQLALCHEMY_DATABASE_URL = 'sqlite:///your_database.db'
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5433/TIK"
+
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
+'''engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+)'''
+#engine = create_engine(SQLALCHEMY_DATABASE_URL,echo = True, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+#with engine.connect() as connection:
+#    connection.execute("SELECT load_extension('mod_spatialite');")
+# = databases.Database(SQLALCHEMY_DATABASE_URL)
